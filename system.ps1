48295be8fc92e889fa66f2a95c16b69550c47ffc
patrikbeno <patrikbeno@gmail.com>
2018-04-19 12:01:18 +0200

    Update system.ps1

--- system.ps1
+++ system.ps1
@@ -3,4 +3,4 @@ iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/in
 choco install -y cygwin conemu
 cmd /c c:\tools\cygwin\cygwinsetup.exe -q -P curl,wget,git
 c:\tools\cygwin\bin\bash.exe -lc "cd /tmp && wget rawgit.com/transcode-open/apt-cyg/master/apt-cyg && install apt-cyg /bin"
-c:\tools\cygwin\bin\bash.exe -lc "curl -sL rawgit.com/jrevolt/devsetup/blob/master/user.sh | bash"
+c:\tools\cygwin\bin\bash.exe -lc "curl -sL rawgit.com/jrevolt/devsetup/master/user.sh | bash"
