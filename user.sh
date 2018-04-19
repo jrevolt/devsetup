d932273e931c938e05357651552fd99d097cf723
patrikbeno <patrikbeno@gmail.com>
2018-04-19 10:35:40 +0200

    Update user.sh

--- user.sh
+++ user.sh
@@ -3,6 +3,10 @@
 if [[ -d $HOME ]]; then
   WHOME="/c/Users/$(whoami)"
   echo "Relocating $HOME -> $WHOME"
+  timestamp="$(date +%Y%m%d-%H%M%S)"
+  for i in $(find -type f); do
+    [ -f "${WHOME}/${i}" ] && mv -v "${WHOME}/${i}" "${WHOME}/${i}.${timestamp}"
+  done
   tar cv . | tar x -C $WHOME 
   cd /
   rm -rf $HOME
