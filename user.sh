a904616f709b0bae77cd0471e15f9ad07dd7bdc7
patrikbeno <patrikbeno@gmail.com>
2018-04-19 10:46:33 +0200

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-if [[ -d $HOME ]]; then
-  cd $HOME
-  WHOME="/cygdrive/c/Users/$(whoami)"
-  echo "Relocating $HOME -> $WHOME"
-  timestamp="$(date +%Y%m%d-%H%M%S)"
-  for i in $(find -type f); do
-    [ -f "${WHOME}/${i}" ] && mv -v "${WHOME}/${i}" "${WHOME}/${i}.${timestamp}"
-  done
-  tar cv . | tar x -C $WHOME 
-  cd /
-  rm -rf $HOME
-  ln -s $WHOME $HOME
-  ls -l $HOME
-else
-  echo "$HOME is not a directory. Assuming it is already relocated"
+if [ -L $HOME ]; then
+  echo "$HOME seems to be already relocated:"
+  ls -lad $HOME
+  exit
 fi
+
+cd $HOME
+WHOME="/cygdrive/c/Users/$(whoami)"
+echo "Relocating $HOME -> $WHOME"
+timestamp="$(date +%Y%m%d-%H%M%S)"
+for i in $(find -type f); do
+  [ -f "${WHOME}/${i}" ] && mv -v "${WHOME}/${i}" "${WHOME}/${i}.${timestamp}"
+done
+tar cv . | tar x -C $WHOME 
+cd /
+rm -rf $HOME
+ln -s $WHOME $HOME
+ls -lad $HOME
 
