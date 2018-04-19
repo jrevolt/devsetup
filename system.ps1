e1fd4023c4e56a07b490d58a28ff0fe042eb0991
patrikbeno <patrikbeno@gmail.com>
2018-04-19 08:07:25 +0200

    Update system.ps1

--- system.ps1
+++ system.ps1
@@ -1,5 +1,5 @@
 #Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://goo.gl/hx3juk'))
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install -y cygwin conemu
-cmd /c c:\tools\cygwin\cygwinsetup.exe -q -P curl,wget,git
+start /wait cmd /c c:\tools\cygwin\cygwinsetup.exe -q -P curl,wget,git
 cygwin
