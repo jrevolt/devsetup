0ce13ee9fe5143c9a9b8cca95c2730ba5a071cc9
patrikbeno <patrikbeno@gmail.com>
2018-04-19 21:58:34 +0200

    Update system.ps1

--- system.ps1
+++ system.ps1
@@ -1,6 +1,8 @@
 #Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://goo.gl/hx3juk'))
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install -y cygwin conemu
-cmd /c c:\tools\cygwin\cygwinsetup.exe -q -P curl,wget,git
-c:\tools\cygwin\bin\bash.exe -lc "cd /tmp && wget rawgit.com/transcode-open/apt-cyg/master/apt-cyg && install apt-cyg /bin"
+cmd /c c:\tools\cygwin\cygwinsetup.exe -q -P curl
+c:\tools\cygwin\bin\bash.exe -lc "curl -sL rawgit.com/transcode-open/apt-cyg/master/apt-cyg > /bin/apt-cyg && chmod +x /bin/apt-cyg"
 c:\tools\cygwin\bin\bash.exe -lc "curl -sL rawgit.com/jrevolt/devsetup/master/user.sh | bash"
+c:\tools\cygwin\bin\bash.exe -lc "apt-cyg install wget git zsh"
+c:\tools\cygwin\bin\bash.exe -lc "curl -sL rawgit.com/robbyrussell/oh-my-zsh/master/tools/install.sh | bash"
