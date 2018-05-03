e536b3eb8a2982e96a82c8b5dab02c8b9efa8207
patrikbeno <patrikbeno@gmail.com>
2018-05-03 12:52:26 +0200

    Update system.ps1

--- system.ps1
+++ system.ps1
@@ -1,5 +1,16 @@
 #Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://goo.gl/hx3juk'))
+
+# install Chocolatey
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# install Cygwin & ConEmu
 choco install -y cygwin conemu
-c:\tools\cygwin\bin\bash.exe -lc "rm -rf /http*; /cygdrive/c/tools/cygwin/cygwinsetup.exe -q --wait --packages curl,wget"
+
+# update Cygwin setup
+Invoke-WebRequest -Uri "https://www.cygwin.com/setup-x86_64.exe" -OutFile c:\tools\cygwin\cygwinsetup.exe
+
+# install missing core packages
+c:\tools\cygwin\bin\bash.exe -lc "rm -rf /http*; /cygdrive/c/tools/cygwin/cygwinsetup.exe -q --packages curl,wget"
+
+# run customizations
 c:\tools\cygwin\bin\bash.exe -lc "curl -sL rawgit.com/jrevolt/devsetup/master/custom.sh | bash"
